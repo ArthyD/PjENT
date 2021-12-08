b199f12499b8912eaffad901bcf90bfd0a249f3b
ArthyD <arthur.docquois@wanadoo.fr>
2021-12-08 19:55:32 +0100

    deuxieme commit

--- website/views.py
+++ website/views.py
@@ -4,11 +4,24 @@ import json
 
 views = Blueprint('views',__name__)
 
-@views.route('/')
+@views.route('/',methods=['POST','GET'])
 def home():
-    
     f = open(os.path.dirname(__file__)+'/config.json')
     configJson = json.load(f)
-    print(configJson)
+    currentlySelected = configJson.pop("SelectedConfig",None)
     f.close()
-    return render_template("mainPage.html", configJson = configJson)
+    if request.method == 'POST':
+        configName = request.form.get("button")
+        f = open(os.path.dirname(__file__)+'/config.json','w')
+        configSelected= {"SelectedConfig":configName}
+        configJson.update(configSelected)
+        print(configJson)
+        json.dump(configJson,f)
+        currentlySelected = configJson.pop("SelectedConfig",None)
+        f.close()
+    return render_template("mainPage.html", configJson = configJson, current = currentlySelected)
+
+@views.route('/controle',methods=['POST','GET'])
+def controle():
+    return render_template("controle.html")
+
