4ed2c3289979bb264a663887209fe335beb46d60
ArthurD <74678288+ArthyD@users.noreply.github.com>
2021-12-14 17:49:07 +0100

    Add files via upload

--- pilotageServo.py
+++ pilotageServo.py
@@ -13,21 +13,11 @@ def ConvertStringToByte(src):
 
 def main(args):
     I2Cbus = smbus.SMBus(1)
-    with smbus.SMBus(1) as I2Cbus :
-        slaveAddress = I2C_SLAVE_ADDRESS
-        servoSelect = input("Which Servo (1-2) :")
-        
-        ByteToSend = servoSelect
-        I2Cbus.write_i2c_block_data(slaveAddress, 0x00, ByteToSend)
-        time.sleep(0.5)
-        while True:
-            try :
-                data= I2Cbus.read_i2c_block_data(slaveAddress,0x00,16)
-                print("recieve from slave:")
-                print(data)
-            except:
-                print("remote i/o error")
-                time.sleep(0.5)
+    slaveAddress = I2C_SLAVE_ADDRESS
+    ByteToSend = ConvertStringToByte("servo1")
+    I2Cbus.write_i2c_block_data(slaveAddress, 0x00, ByteToSend)
+    time.sleep(0.5)
+       
     return 0
 
 if __name__ == '__main__':
@@ -35,4 +25,4 @@ if __name__ == '__main__':
         main(sys.argv)
     except KeyboardInterrupt:
         print("program stopped")
-    input()
+    
